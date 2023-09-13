361ccf8c2793b24467ea8b8a6adad841b15ccec3
zeyad.alasaad <zeyad.T.alasaad@outlook.com>
2023-09-14 01:15:26 +0300

    re-deploy

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <code className="font-mono font-bold">
-            Site will be live soon ...
+            Site will be live soon ....
           </code>
         </p>
       </div>
