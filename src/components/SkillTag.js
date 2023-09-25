ae73447a16efc03b90aa2c2d01436b35fcbf6141
zeyad.alasaad <zeyad.T.alasaad@outlook.com>
2023-09-25 21:38:01 +0300

    fix skills layout shift

--- src/components/SkillTag.js
+++ src/components/SkillTag.js
@@ -3,7 +3,7 @@ const SkillTag = ({ title, icon }) => {
     <div
       className={`btn btn-outline px-4 flex flex-nowrap items-center justify-center w-min m-2`}
     >
-      <span className="mr-1">{icon}</span>
+      <span className="mr-1 w-[20px] h-[20px]">{icon}</span>
       <span className="whitespace-nowrap">{title}</span>
     </div>
   );
