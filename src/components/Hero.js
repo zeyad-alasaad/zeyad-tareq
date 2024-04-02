6c971dcf00d0e83a34a958d583fc4897d82b898e
Zeyad Tareq Alas'ad <62501728+zeyad-alasaad@users.noreply.github.com>
2024-04-02 22:19:22 +0300

    hero section update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,23 +1,16 @@
-const Hero = () => {
-  return (
-    <div className="mockup-browser border border-base-content w-full">
-      <div className="mockup-browser-toolbar">
-        <div className="input border border-base-content">
-          https://zeyadtareq.com
-        </div>
-      </div>
-      <div className="flex justify-center py-4 border-t border-base-content text-2xl">
-        Hello!
-      </div>
-      <div className="flex justify-center pb-4 text-3xl whitespace-break-spaces">
-        I am <span className="text-success">Zeyad</span>
-      </div>
-      <div className="flex justify-center px-4 pb-6 text-xl whitespace-break-spaces">
-        Driven Frontend Engineer 🚀, Breathing Life into Web Visions ✨, Expert
-        in React and Next 💻
-      </div>
+const Hero = () => (
+  <div className="mockup-window border border-base-content w-full">
+    <div className="flex justify-center py-4 border-t border-base-content text-2xl">
+      Hello!
     </div>
-  );
-};
+    <div className="flex justify-center pb-4 text-3xl whitespace-break-spaces">
+      I am <span className="text-success">Zeyad</span>
+    </div>
+    <div className="flex justify-center px-4 pb-6 text-xl whitespace-break-spaces">
+      Driven Frontend Engineer 🚀, Breathing Life into Web Visions ✨, Expert in
+      React and Next 💻
+    </div>
+  </div>
+);
 
 export default Hero;
